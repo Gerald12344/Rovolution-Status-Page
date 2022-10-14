3d899f1a288ee2ea44e7bbff00b7511596be1160
Gerald12344 <harvey.randall@aol.com>
2022-10-14 15:12:33 +0100

    Added cors

--- pages/api/down.ts
+++ pages/api/down.ts
@@ -1,7 +1,14 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import NextCors from "nextjs-cors";
 import { getManyFromUtilsFolder } from "../../server/database/MongoMethods";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    await NextCors(req, res, {
+        // Options
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        origin: '*',
+        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    });
     const down = await getManyFromUtilsFolder('whats_down', {});
     res.status(200).json({ down })
 }
